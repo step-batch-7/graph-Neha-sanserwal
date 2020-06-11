33d23edca13b4f7377b4cd47c3706ae9b1a95c27
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2020-06-11 06:59:17 +0530

    removed comments

--- src/graph.js
+++ src/graph.js
@@ -4,26 +4,6 @@
 // To => to
 // Should return true.
 
-// const search = function (links, source, target, visitedNodes) {
-//   if (!(source in links)) {
-//     return false;
-//   }
-//   if (source == target && !links[source].includes(target)) {
-//     return false;
-//   }
-//   if (links[source].includes(target)) {
-//     visitedNodes = [];
-//     return true;
-//   }
-//   visitedNodes.push(source);
-//   for (const newSource of links[source]) {
-//     if (!visitedNodes.includes(newSource)) {
-//       return search(links, newSource, target, visitedNodes);
-//     }
-//   }
-//   return false;
-// };
-
 const createGraphList = function (data) {
   let list = {};
   for (const [source, destination] of data) {
@@ -68,4 +48,5 @@ const bfs = function (pairs, source, target) {
   let links = createGraphList(pairs);
   return search(links, source, target, visitedNodes, queue);
 };
+
 module.exports = { bfs, createGraphList };
